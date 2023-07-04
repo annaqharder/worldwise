4c42f38702f76895e0c8eeb9cda26b3a4151dfd3
Anna Harder <annaqharder@gmail.com>
2023-07-04 11:21:28 -0600

    added starter file

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>NAV</p>
+    </div>
+  );
+}
+
+export default AppLayout;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ function Homepage() {
   return (
     <div>
       <PageNav />
+
       <h1>WorldWise</h1>
     </div>
   );
