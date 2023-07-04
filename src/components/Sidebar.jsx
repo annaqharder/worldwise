434e8b99ecf74ccdc750b44fdd25516f806489df
Anna Harder <annaqharder@gmail.com>
2023-07-04 12:01:53 -0600

    implemented nested routes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
 import Logo from "./Logo";
@@ -8,7 +9,8 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of Cities</p>
+
+      <Outlet />
       <Footer />
     </div>
   );
