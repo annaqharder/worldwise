6b27a07166151eae26a2b284926c51d19997612f
Anna Harder <annaqharder@gmail.com>
2023-07-05 17:36:00 -0600

    made CitiesProvider.jsx

---
+++ src/contexts/CitiesProvider.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+CitiesProvider.propTypes = {
+  children: PropTypes.object,
+};
+
+const CitiesContext = createContext();
+const BASE_URL = `http://localhost:8000`;
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert(`There was an error loading data...`);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
