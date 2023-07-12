f4a398dd9630fad733c81608ef2671e1511292a7
Anna Harder <annaqharder@gmail.com>
2023-07-12 17:28:20 -0600

    added logout functionality

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/FakeAuthProvider";
 import styles from "./User.module.css";
 
-const FAKE_USER = {
-  name: "Anna",
-  email: "anna@example.com",
-  password: "123",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
 function User() {
-  const user = FAKE_USER;
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
 
-  function handleClick() {}
+  function handleClick() {
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
